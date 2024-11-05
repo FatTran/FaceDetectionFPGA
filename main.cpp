14fa9e8b17d33077e0b61f4ed4adbcd19faffa21
FatTran <123949287+FatTran@users.noreply.github.com>
2024-11-05 20:07:33 +0700

    chỉnh hàm main

--- main.cpp
+++ main.cpp
@@ -1,22 +1,28 @@
-#include <opencv2/opencv.hpp>
-#include <iostream>
-#include <vector>
 #include "haar.h"
+#include <chrono>
 
-
-//C:/Users/ADMIN/Downloads/lfw_funneled/Colin_Prescot/Colin_Prescot_0001.jpg
 int main() {
-    cv::Mat inputImage = cv::imread("C:/Users/ADMIN/Downloads/lfw_funneled/Dino_de_Laurentis/Dino_de_Laurentis_0002.jpg"); // Load your test image here
-
-    if (inputImage.empty()) {
-        std::cerr << "Failed to load image." << std::endl;
+    std::ios::sync_with_stdio(false);
+    cv::Mat colorImage = cv::imread("C:/Users/Phat/Downloads/29_(1).jpg");
+    if (colorImage.empty()) {
+        std::cerr << "khong the mo anh dau vao" << std::endl;
         return -1;
     }
-
-   
-    processImage(inputImage);
-
-    cv::imshow("Face Detection", inputImage);
+    cv::Mat grayScaleImage;
+    cv::Mat integralImage;
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+    detectFace(colorImage, 1.25);
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
+    cv::imshow("Output image", colorImage);
+    //scanImageWithVariableWindow(colorImage);
+    //convertToGrayscale(colorImage, grayScaleImage);
+    //createIntegralImage(grayScaleImage, integralImage);
+    //cascadeClassifier()
+    //detectFace(integralImage, 0, 0, 0);
+    //cv::imshow("Ketqua", colorImage);
     cv::waitKey(0);
+    //detectFace(integralImage, 0);
+    //cv::imwrite("D:/UTE/Advanced_Topic/SW/Output/img.jpg", colorImage);
     return 0;
 }
