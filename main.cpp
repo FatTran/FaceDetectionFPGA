3b02903f0ea1c5e30c5a000ab6433867a49de2bb
Phan Minh Đức <113233216+phanminhduc241202@users.noreply.github.com>
2024-11-15 21:52:58 +0700

    Duc GroupBoundingBoxes

--- main.cpp
+++ main.cpp
@@ -1,22 +1,42 @@
-#include <opencv2/opencv.hpp>
-#include <iostream>
-#include <vector>
 #include "haar.h"
+#include <chrono>
 
-
-//C:/Users/ADMIN/Downloads/lfw_funneled/Colin_Prescot/Colin_Prescot_0001.jpg
 int main() {
-    cv::Mat inputImage = cv::imread("C:/Users/ADMIN/Downloads/lfw_funneled/Dino_de_Laurentis/Dino_de_Laurentis_0002.jpg"); // Load your test image here
-
-    if (inputImage.empty()) {
-        std::cerr << "Failed to load image." << std::endl;
+    //std::ios::sync_with_stdio(false);
+    cv::Mat colorImage = cv::imread("C:/MD_Documents/Avad_Topic/IN_OUT/Input/test/31-40/29.jpg");
+    if (colorImage.empty()) {
+        std::cerr << "khong the mo anh dau vao" << std::endl;
         return -1;
     }
-
-   
-    processImage(inputImage);
-
-    cv::imshow("Face Detection", inputImage);
+   /* cv::VideoCapture cap(0);
+    if (!cap.isOpened()) {
+        std::cout << "Can't open device" << std::endl;
+    }*/
+    //bool done = false;
+    //cv::Mat cameraImage;
+    //cv::Mat scaledImage;
+   /* while (!done) {
+        cap >> cameraImage;
+        if (cameraImage.empty()) {
+            break;
+        }
+        cv::resize(cameraImage, scaledImage, cv::Size(), 0.5, 0.5, cv::INTER_AREA);
+        std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+        detectFace(scaledImage, 1.2);
+        std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+        std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
+        cv::imshow("Output image", scaledImage);
+        int key = cv::waitKey(1);
+        if (key == 'x') {
+            break;
+        }
+    }*/
+    //cv::resize(colorImage, scaledImage, cv::Size(), 0.5, 0.5, cv::INTER_AREA);
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+    detectFace(colorImage, 1.2);
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
+    cv::imshow("Output image", colorImage);
     cv::waitKey(0);
     return 0;
 }
